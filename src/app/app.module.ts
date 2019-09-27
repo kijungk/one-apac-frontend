f8cd63269d6fa0b80fc920af16cacca7dcadf412
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-26 16:44:37 -1000

    generated header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { EventsComponent } from './components/events/events.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    EventsComponent
+    EventsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
