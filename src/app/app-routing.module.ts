fcfe2a93bb1b1ba0fa78a9d95fe8edc1ef0e06dd
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-27 11:51:42 -1000

    made a route for statistics component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'statistics', component: StatisticsComponent }
 ];
 
 @NgModule({
