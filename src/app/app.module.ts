3f43dbb11abd3f008d0588f5745faa9dd9a2fba6
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-26 16:42:01 -1000

    injected login and events component into application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { EventsComponent } from './components/events/events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
