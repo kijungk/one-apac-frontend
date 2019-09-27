90b293eb478eb84240fe878138dc4b6bd1d7d5bb
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-27 11:53:20 -1000

    added broadcast component into routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { BroadcastComponent } from './pages/broadcast/broadcast.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'broadcast', component: BroadcastComponent }
 ];
 
 @NgModule({
