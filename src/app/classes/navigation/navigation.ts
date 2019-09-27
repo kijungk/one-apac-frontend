7630afc405b78102993ccf56d7e5d453eb0fb438
Ki Jung Kim <kijungkim@eworldes.com>
2019-09-27 11:36:06 -1000

    made navigation class

---
+++ src/app/classes/navigation/navigation.ts
@@ -0,0 +1,9 @@
+export class Navigation {
+  description: string;
+  iconClass: string;
+
+  constructor(description: string, iconClass: string) {
+    this.description = description;
+    this.iconClass = iconClass;
+  }
+}
