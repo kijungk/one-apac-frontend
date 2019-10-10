f0686159f677b5b921f5258727bb1181f971f63e
Ki Jung Kim <kijungkim@eworldes.com>
2019-10-09 15:05:39 -1000

    created reference data binding for login status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { routeTransitionAnimation } from './animations/route-transition/animation';
+import { Administrator } from './classes/administrator/administrator';
+import { AdministratorService } from './services/administrator/administrator.service';
+import { loginAnimation } from './animations/login/animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [routeTransitionAnimation]
+  animations: [routeTransitionAnimation, loginAnimation]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private administrator: Administrator;
+
+  constructor(
+    private administratorService: AdministratorService
+  ) { }
+
+  ngOnInit() {
+    this.administrator = this.administratorService.administrator;
+  }
+
   private prepareRoute(outlet: RouterOutlet): boolean {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
   }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Administrator } from '../../classes/administrator/administrator';
+import { AdministratorService } from '../../services/administrator/administrator.service';
+import { loginAnimation } from '../../animations/login/animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [loginAnimation]
 })
 export class HomeComponent implements OnInit {
+  private administrator: Administrator;
 
-  constructor() { }
+  constructor(
+    private administratorService: AdministratorService
+  ) { }
 
   ngOnInit() {
+    this.administrator = this.administratorService.administrator;
   }
-
 }
