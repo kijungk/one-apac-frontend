44c49b0f1196b51649d06348fbd193c3d4e986d5
Ki Jung Kim <kijungkim@eworldes.com>
2019-10-18 11:45:59 -1000

    created some basic logic to retrieve statistics

--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Administrator } from '../../classes/administrator/administrator';
+import { View } from '../../classes/view/view';
+import { Vote } from '../../classes/vote/vote';
+import { AdministratorService } from '../../services/administrator/administrator.service';
+import { StatisticsService } from '../../services/statistics/statistics.service';
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +12,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
+  administrator: Administrator;
+  views: Observable<View[]>;
+  votes: Observable<Vote[]>;
 
-  constructor() { }
+  constructor(
+    private administratorService: AdministratorService,
+    private statisticsService: StatisticsService
+  ) { }
 
   ngOnInit() {
+    this.administrator = this.administratorService.administrator;
+
+    this.views = this.statisticsService.getViews(this.administrator.eventId);
   }
 
 }
